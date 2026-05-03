7196278a34220cbf3c40356c0ece38fb823214da
kuttyb <kutty.banerjee@gmail.com>
2026-05-03 22:19:13 +0000

    Changes

--- src/hello.jl
+++ src/hello.jl
@@ -1,12 +1,23 @@
-function square(x::Integer)
-    x * x
+abstract type A end
+abstract type B end
+
+struct A1 <: A
+    value::Int
 end
 
-function square(x::AbstractFloat)
-    x + x
+struct B1 <: B
+    value::Float64
 end
 
-const i = 7
-const f = 2.5
-println("square(Integer) = ", square(i))
-println("square(Float) = ", square(f))
+function process(x::A)
+    x.value + 1
+end
+
+function process(x::B)
+    x.value * 2.0
+end
+
+const a = A1(7)
+const b = B1(2.5)
+println("process(A1) = ", process(a))
+println("process(B1) = ", process(b))
